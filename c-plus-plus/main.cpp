e7939756c7ad8ea539f037541039a3dade1060a3
web1992 <554933654@qq.com>
2021-01-23 17:16:57 +0800

    add

--- c-plus-plus/main.cpp
+++ c-plus-plus/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  c-plus-plus
 //
-//  Created by Lucy on 2021/1/23.
+//  Created by web2992 on 2021/1/23.
 //
 
 #include <iostream>
@@ -10,5 +10,8 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    int  a = 100;
+    std::cout << a;
+    std::cout << std::endl;
     return 0;
 }
