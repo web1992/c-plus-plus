6329708515def77c4d6caabf34bfdcb8b4ee75d0
web1992 <554933654@qq.com>
2021-01-23 16:55:21 +0800

    Initial Commit

---
+++ c-plus-plus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c-plus-plus
+//
+//  Created by Lucy on 2021/1/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
