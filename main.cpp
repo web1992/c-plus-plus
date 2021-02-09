7927248169ca00fe18a48d042afdab70a0624e25
web1992 <554933654@qq.com>
2021-02-09 15:07:30 +0800

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -11,13 +11,17 @@
 int main(int argc, const char *argv[])
 {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "argc count =" << argc << std::endl;
+    std::cout << "argv[0] =" << *argv[0] << std::endl;
     int a = 100;
     std::cout << a;
     std::cout << std::endl;
     std::string name;
+
+    std::cout << "Enter your name:" << std::endl;
     std::cin >> name;
-    std::cout << "Your name is :" << name;
+    std::cout << "Your name is :" << name << std::endl;
 
     return 0;
 }
