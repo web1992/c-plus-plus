c25a3f9fbf3d157ccb942305009d931896832beb
web1992 <554933654@qq.com>
2021-02-13 23:27:03 +0800

    fix chars

--- chars/chars.cpp
+++ chars/chars.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <cstring>
 
-int main(){
+int main()
+{
 
-    char names[]={'1','2'};
+    char names[] = {'1', '2','\0','\0'};
+    char title[] = "Java\0";
 
     std::cout << "names is " << names << std::endl;
+    std::cout << "title is " << title << std::endl;
 
+    std::cout << "names sizeof " << sizeof names << std::endl;
+    std::cout << "title sizeof " << sizeof title << std::endl;
+
+    std::cout << "names len is " << strlen(names) << std::endl;
+    std::cout << "title len is " << strlen(title) << std::endl;
     return 0;
 }
