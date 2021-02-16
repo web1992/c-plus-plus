818e5f3d18b75e9dec65fdd6c4c70a4c967be3d4
web1992 <554933654@qq.com>
2021-02-16 21:35:50 +0800

    fix struct new

--- structs/struct_new.cpp
+++ structs/struct_new.cpp
@@ -6,6 +6,8 @@ struct people
     int age;
 };
 
+// *pp 是指针，需要使用 箭头成员运算符
+// -> 箭头成员运算符
 int main()
 {
 
@@ -16,6 +18,8 @@ int main()
 
     std::cout << "name = " << pp->name << std::endl;
     std::cout << "age = " << pp->age << std::endl;
-    
+
+    std::cout << "(*pp).name = " << (*pp).name << std::endl;
+    std::cout << "(*pp).age = " << (*pp).age << std::endl;
     return 0;
 }
