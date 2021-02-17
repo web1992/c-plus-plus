7637302ea5371ae9002fb80e57a78a1de810a77e
web1992 <554933654@qq.com>
2021-02-17 15:53:23 +0800

    add func

---
+++ funcs/calling.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void sample();
+
+int main()
+{
+    using namespace std;
+
+    sample();
+    return 0;
+}
+
+void sample()
+{
+
+    using namespace std;
+    cout << "I'm but a sample function.\n";
+}
