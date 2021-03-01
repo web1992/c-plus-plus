2b484d84866abe6d9a4175c4ebc5ba0e0d2f09c3
web1992 <554933654@qq.com>
2021-03-01 18:12:01 +0800

    add template

--- funcs/template.cpp
+++ funcs/template.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
 
 template <class T>
 void swap(T *a, T *b);
+
+template <class T>
+void error(T a);
+
+template <class T>
+void error(T a, int num);
 
 int main()
 {
@@ -16,6 +23,10 @@ int main()
     swap(&d1, &d2);
     std::cout << "d1=" << d1 << " d2=" << d2 << std::endl;
 
+    std::string err = "I am error";
+    error(err);
+    error(err, 666);
+
     return 0;
 }
 
@@ -26,4 +37,17 @@ void swap(T *a, T *b)
     t = *a,
     *a = *b;
     *b = t;
+}
+
+// 模板重载
+template <class T>
+void error(T a)
+{
+    std::cout << "error:" << a << std::endl;
+}
+
+template <class T>
+void error(T a, int num)
+{
+    std::cout << "error:" << a << " num:" << num << std::endl;
 }
