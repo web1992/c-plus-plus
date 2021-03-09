c5816c010499750ea1d7767696d6af4b3983da5e
web1992 <554933654@qq.com>
2021-03-09 17:49:09 +0800

    fix time0

--- classs/time0/time0.h
+++ classs/time0/time0.h
@@ -17,6 +17,7 @@ public:
     void AddMin(int m);
     void Rest(int h = 0, int m = 0);
     Time operator+(const Time &t) const;
+    //friend Time operator+(const Time &t1, const Time &t2); // use of overloaded operator '+' is ambiguous (with operand types 'Time' and 'Time')
     Time operator-(const Time &t) const;
     Time operator*(double n) const;
     void Show();
