618c3069c82c2f0b55472d55eb733acb539aab31
web1992 <554933654@qq.com>
2021-03-13 22:25:25 +0800

    add operator=

--- strings/string_bad/string_bad.cpp
+++ strings/string_bad/string_bad.cpp
@@ -3,7 +3,18 @@
 
 class StringBad
 {
+    char *str;
+    int len;
     StringBad &StringBad::operator=(StringBad &s)
     {
+        if (this == &s)
+        {
+            return s;
+        }
+        delete[] str;
+        len = s.len;
+        char *st = new char[len + 1];
+        std::strcpy(str, s.str);
+        return *this;
     }
 };
