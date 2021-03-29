1bba2940e258e3db2d7203d64c9f218810a78c4a
web1992 <554933654@qq.com>
2021-03-29 09:56:41 +0800

    add hangman

---
+++ classs/table0/rateplayer.cpp
@@ -0,0 +1,8 @@
+#include "tablenn0.h"
+
+class RatePlayer : public TableTennisPlayer
+{
+private:
+public:
+    RatePlayer();
+};
---
+++ classs/template/valarray0.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <valarray>
+
+int main() {
+
+    using namespace std;
+
+    valarray<int> intValues;
+    valarray<int> v2(8);
+    valarray<int> v3(10, 8);
+
+    return 0;
+}
