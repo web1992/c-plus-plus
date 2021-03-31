8783e2ec9f50a87995d82db82993237fceccdacd
web1992 <554933654@qq.com>
2021-03-31 09:37:33 +0800

    fix iterator insert

--- iterator/inserts.cpp
+++ iterator/inserts.cpp
@@ -15,16 +15,16 @@ int main()
 
     vector<string> words(4);
 
-    copy(s1, s1 + 4, words.begin());
-    for_each(words.begin(), words.end(), output);
+    std::copy(s1, s1 + 4, words.begin());
+    std::for_each(words.begin(), words.end(), output);
     cout << endl;
 
-    copy(s2, s2 + 2, back_insert_iterator<vector<string> >(words));
-    for_each(words.begin(), words.end(), output);
+    std::copy(s2, s2 + 2, back_insert_iterator<vector<string> >(words));
+    std::for_each(words.begin(), words.end(), output);
     cout << endl;
 
-    copy(s3, s3 + 2, insert_iterator<vector<string> >(words, words.begin()));
-    for_each(words.begin(), words.end(), output);
+    std::copy(s3, s3 + 2, insert_iterator<vector<string> >(words, words.begin()));
+    std::for_each(words.begin(), words.end(), output);
     cout << endl;
 
     return 0;
