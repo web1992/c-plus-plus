c27e375e4b15d7f7eb9f7894de4a853be6539a4b
web1992 <554933654@qq.com>
2022-05-02 14:19:05 +0800

    fix sizeof test

--- sizeof/sizeof_test.cpp
+++ sizeof/sizeof_test.cpp
@@ -2,16 +2,40 @@
 
 struct S
 {
-    int a;// 8
-    char b;
-    int c;
+    int a;  // 4
+    char b; // 1+3,此处会进行3字节对齐
+    int c;  // 4
 };
 
+struct test
+{
+    char a; // 1
+    char b; // 1
+};
+
+struct test2
+{
+    char a; // 1+3
+    int aa; // 4
+    char b; // 1+3
+    int cc; // 4
+};
+
+// 字节对齐的测试
 int main()
 {
 
+    int a;   // 4
+    char ch; // 1
+    std::cout << "sizeof a=" << sizeof(a) << std::endl;
+    std::cout << "sizeof ch=" << sizeof(ch) << std::endl;
+    
     // sizeof S=12
     std::cout << "sizeof S=" << sizeof(S) << std::endl;
+    // sizeof test=2
+    std::cout << "sizeof test=" << sizeof(test) << std::endl;
+    // sizeof test2=16
+    std::cout << "sizeof test2=" << sizeof(test2) << std::endl;
 
     return 0;
 }
